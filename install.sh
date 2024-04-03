6a9505a26a1e7e5398c08bb9dabfaf3e1d6dd4c4
ADIRMA <139200385+irmabot23@users.noreply.github.com>
2024-04-03 08:05:16 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-git config --global user.email "farhanart010503@gmail.com"
-git config --global user.name "FarhannnnX"
+git config --global user.email "irmahelper23@gmail.com"
+git config --global user.name "irmabot23"
 git init
 git add *
 git commit -m "new update"
 git branch -M master
-git remote add origin https://github.com/FarhannnnX/restAPIs.git
+git remote add origin https://github.com/irmabot23/ahayapis.git
 git push -u origin master
